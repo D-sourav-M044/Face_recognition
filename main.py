5704b55e68fc9ce857ecd0142ee55166e268f54a
D.sourav <45461265+D-sourav-M044@users.noreply.github.com>
2019-12-07 02:40:36 +0600

    Update main.py
    adding background

--- main.py
+++ main.py
@@ -2,12 +2,21 @@ import sys
 import qdarkstyle
 from PyQt5.QtWidgets import QApplication
 from MainWindow import MainWindow
-
+stylesheet = """
+    MainWindow {
+        background-image: url("background.jpg");
+        background-repeat: no-repeat; 
+        background-position: center;
+    }
+"""
 
 def main():
     app = QApplication(sys.argv)
     main_window = MainWindow()
-    app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
+    main_window.resize(640, 600)
+   #app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
+    app.setStyleSheet(stylesheet)
     main_window.show()
     sys.exit(app.exec_())
 main()
+
